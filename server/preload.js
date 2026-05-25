df81e71463077bb4aec7b7377ee74ef7994764b8
a <102840087+kingdudely@users.noreply.github.com>
2026-05-25 17:48:30 -0400

    Delete server/preload.js

--- server/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-console.log('[preload loaded]')
-console.log(JSON.stringify(Object.keys(require("electron")), null, 2))
-
-contextBridge.exposeInMainWorld('api', {
-  getSources: () => ipcRenderer.invoke('GET_SOURCES'),
-  sendControl: (msg) => ipcRenderer.send('control', msg)
-})
