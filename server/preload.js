56280857c811948907fad2d96ff0e4a379af925c
a <102840087+kingdudely@users.noreply.github.com>
2026-05-25 14:39:19 -0400

    Update preload.js

--- server/preload.js
+++ server/preload.js
@@ -1,9 +1,9 @@
-const { contextBridge, desktopCapturer, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 console.log('[preload loaded]')
 console.log(JSON.stringify(Object.keys(require("electron")), null, 2))
 
 contextBridge.exposeInMainWorld('api', {
-  getSources: () => desktopCapturer.getSources({ types: ['screen'] }),
+  getSources: () => ipcRenderer.invoke('GET_SOURCES'),
   sendControl: (msg) => ipcRenderer.send('control', msg)
 })
