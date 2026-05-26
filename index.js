86fe8a777a0b2a2a831836fd84f0d3f5db743ebb
a <102840087+kingdudely@users.noreply.github.com>
2026-05-26 11:44:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { env } from "node:process"
 import serveIndex from "serve-index";
 import express from "express";
-import { normalize } from "node:path";
+import { normalize, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { stat } from 'node:fs/promises';
 
@@ -11,21 +11,48 @@ const isDirectory = async (filePath) => (await stat(filePath)).isDirectory();
 
 const app = express();
 
+import { execSync } from 'node:child_process';
+import serveIndex from "serve-index";
+import express from "express";
+import { normalize, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { stat } from 'node:fs/promises';
+
+const { TUNNEL_URL, PORT } = process.env;
+const getRelativePath = (filePath) => normalize(fileURLToPath(import.meta.resolve(filePath)));
+const isDirectory = async (filePath) => (await stat(filePath)).isDirectory();
+
+const getDrives = () => {
+    if (process.platform !== 'win32') return null;
+    return execSync('wmic logicaldisk get name')
+        .toString()
+        .split('\n')
+        .map(s => s.trim())
+        .filter(s => /^[A-Z]:$/.test(s));
+};
+
+const app = express();
+
 app.use(async (request, response, next) => {
-    const requestPath = normalize(decodeURIComponent(request.path.replace(/^\//, '')));
-    console.log(requestPath)
-    console.log(decodeURIComponent(request.path))
-    
+    const requestPath = normalize(decodeURIComponent(request.path.replace(/^\//, ''))) || "/";
+	const drives = getDrives();
+
     try {
-        if (await isDirectory(requestPath)) {
-			return serveIndex(requestPath, { icons: true, root: requestPath })(request, response, next);
-		} else {
-            return response.sendFile(requestPath, next);
-        }
+        if (requestPath === "/" && drives) {
+            return response.send(drives.map(d => `<a href="/${encodeURIComponent(d + '\\')}"> ${d}\\</a>`).join('<br>'));
+        };
+
+		const root = (await isDirectory(requestPath)) ? requestPath : dirname(requestPath);
+		request.url = '/';
+		express.static(root)(request, response, () => serveIndex(root, { icons: true })(request, response, next));
     } catch (error) {
-        console.warn(error);
+        console.warn(error)
         return next();
     }
+});
+
+app.listen(PORT, () => {
+    console.log(`https://${TUNNEL_URL}/${encodeURIComponent(getRelativePath("./public/index.html"))}`);
 });
 
 app.listen(PORT, () => {
