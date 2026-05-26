146951c1ce13785ade0511dedcc9964259fad1c5
a <102840087+kingdudely@users.noreply.github.com>
2026-05-26 09:25:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ const __dirname = import.meta.dirname;
 const app = express();
 const osRoot = path.parse(__dirname).root;
 
-app.use('/', async (req, res, next) => {
-  try {
-    express.static(osRoot)(req, res, next);
-  } catch (err) {
-    next();
-  }
-});
-
-app.use('/', serveIndex(osRoot, { icons: true }));
+// Error: EINVAL: invalid argument, stat 'D:\DumpStack.log.tmp'
+const staticMiddleman = express.static(osRoot);
+const serveIndexMiddleman = serveIndex(osRoot, { icons: true });
+app.use(
+	'/',
+	(req, res, next) => staticMiddleman(req, res, () => next()),
+	(req, res, next) => serveIndexMiddleman(req, res, () => next())
+);
 
 app.listen(PORT, () => {
-  const publicIndex = path.join(__dirname, 'public', 'index.html');
-  console.log(`https://${TUNNEL_URL}/${path.relative(osRoot, publicIndex)}`);
+	const publicIndex = path.join(__dirname, 'public', 'index.html');
+	console.log(`https://${TUNNEL_URL}/${path.relative(osRoot, publicIndex)}`);
 });
