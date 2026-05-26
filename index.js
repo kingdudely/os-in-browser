60a51c5a8911846631b2f6bf8e19e7cdfdc850b5
a <102840087+kingdudely@users.noreply.github.com>
2026-05-26 08:43:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,17 @@
-import { app, BrowserWindow, desktopCapturer } from 'electron'
-import { createServer } from 'node:http'
-import { ExpressPeerServer } from 'peer'
-import { fileURLToPath } from 'node:url'
-import { dirname, join } from 'node:path'
+import process from "node:process";
+import serveIndex from 'serve-index';
 import express from 'express'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const { PASSWORD, TUNNEL_URL, PORT } = process.env;
 
-const expressApp = express()
-const server = createServer(expressApp)
+const __dirname = import.meta.dirname
 
-const peerServer = ExpressPeerServer(server, { path: '/myapp' })
-expressApp.use('/peerjs', peerServer)
+const app = express()
 
-expressApp.use(express.static(join(__dirname, 'public')))
+const osRoot = path.parse(__dirname).root;
 
-function createHostWindow() {
-  const win = new BrowserWindow({
-    show: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+app.use('/', express.static(osRoot), serveIndex(osRoot, { icons: true }));
 
-  win.loadURL('about:blank')
-
-  win.webContents.on('did-finish-load', async () => {
-    const sources = await desktopCapturer.getSources({ types: ['screen'] })
-    const source = sources[0]
-
-    win.webContents.executeJavaScript(`
-      const { Peer } = require('peerjs')
-
-      const peer = new Peer('${process.env.PASSWORD}', {
-        host: 'localhost',
-        port: 8080,
-        path: '/peerjs/myapp'
-      })
-
-      peer.on('open', async () => {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: '${source.id}'
-            }
-          }
-        })
-
-        peer.on('call', call => call.answer(stream))
-      })
-    `).catch(console.warn)
-  })
-}
-
-server.listen(8080, () => {
-  console.log('HTTP + PeerJS broker running on :8080')
-})
-
-app.whenReady().then(() => {
-  createHostWindow()
-})
+app.listen(PORT, () => {
+  console.log(`https://${TUNNEL_URL}`)
+});
