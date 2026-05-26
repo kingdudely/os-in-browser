f98245578febdeb44c6f9b23dced7a471a314a71
a <102840087+kingdudely@users.noreply.github.com>
2026-05-26 10:30:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-import process from "node:process";
-import serveIndex from 'serve-index';
-import express from 'express';
-import path from 'node:path';
+import { env } from "node:process"
+import serveIndex from "serve-index";
+import express from "express";
+import { normalize } from "node:path";
+import { fileURLToPath } from "node:url";
+import { stat } from 'node:fs/promises';
 
 const { PASSWORD, TUNNEL_URL, PORT } = process.env;
-const __dirname = import.meta.dirname;
+const getRelativePath = (filePath) => normalize(fileURLToPath(import.meta.resolve(filePath)));
+const isDirectory = async (filePath) => (await stat(filePath)).isDirectory();
+
 const app = express();
-const osRoot = path.parse(__dirname).root;
 
-console.log(osRoot)
-
-// Error: EINVAL: invalid argument, stat 'D:\DumpStack.log.tmp'
-const staticMiddleman = express.static(osRoot);
-const serveIndexMiddleman = serveIndex(osRoot, { icons: true });
-app.use(
-	'/',
-	(req, res, next) => staticMiddleman(req, res, () => next()),
-	(req, res, next) => serveIndexMiddleman(req, res, () => next())
-);
+app.use(async (request, response, next) => {
+	const requestPath = normalize(decodeURIComponent(request.path));
+	
+	try {
+		if (await isDirectory(requestPath)) {
+			return serveIndex(requestPath, { icons: true })(request, response, next);
+		} else {
+			return response.sendFile(requestPath);
+		}
+	} catch (error) {
+		console.warn(error);
+		return next();
+	}
+});
 
 app.listen(PORT, () => {
-	const publicIndex = path.join(__dirname, 'public', 'index.html');
-	console.log(`https://${TUNNEL_URL}/${path.relative(osRoot, publicIndex)}`);
+	console.log(`https://${TUNNEL_URL}/${encodeURIComponent(getRelativePath("./public/index.html"))}`);
 });
