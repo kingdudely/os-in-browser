87e91f9afa11cf3b114d37f436e768647d3fe4ac
a <102840087+kingdudely@users.noreply.github.com>
2026-05-26 08:56:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import process from "node:process";
-import serveIndex from 'serve-index';
-import express from 'express'
+import process from "node:process"; 
+import serveIndex from 'serve-index'; 
+import express from 'express';
+import path from 'node:path';
 
-const { PASSWORD, TUNNEL_URL, PORT } = process.env;
+const { PASSWORD, TUNNEL_URL, PORT } = process.env; 
+const __dirname = import.meta.dirname;
+const app = express();
 
-const __dirname = import.meta.dirname
+const osRoot = path.parse(__dirname).root; 
+app.use('/', express.static(osRoot), serveIndex(osRoot, { icons: true })); 
 
-const app = express()
-
-const osRoot = path.parse(__dirname).root;
-
-app.use('/', express.static(osRoot), serveIndex(osRoot, { icons: true }));
-
-app.listen(PORT, () => {
-  console.log(`https://${TUNNEL_URL}`)
+app.listen(PORT, () => { 
+  const publicIndex = path.join(__dirname, 'public', 'index.html');
+  console.log(`https://${TUNNEL_URL}/${path.relative(osRoot, publicIndex)}`);
 });
