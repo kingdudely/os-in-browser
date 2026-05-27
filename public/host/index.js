a3ae2c344bb6a5eba4c860f3cb80cdcb13149fb6
a <102840087+kingdudely@users.noreply.github.com>
2026-05-27 15:37:33 -0400

    Create index.js

---
+++ public/host/index.js
@@ -0,0 +1,36 @@
+const status = document.getElementById("status");
+console.log(location.hostname);
+
+const peer = new Peer(, {
+  host: location.hostname,
+  port: location.port,
+  path: "/peerjs"
+});
+
+peer.on("open", id => {
+  console.log("HOST_READY", id);
+
+  status.innerText =
+    "Ready. ID: " + id;
+});
+
+peer.on("call", async call => {
+  console.log("INCOMING_CALL");
+
+  try {
+    const stream =
+      await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true
+      });
+
+    console.log("STREAM_READY");
+
+    call.answer(stream);
+
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+peer.on("error", console.error);
