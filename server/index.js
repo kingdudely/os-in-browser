38469c6619e26c13fe92e1cf5651cf81c6feaa9e
a <102840087+kingdudely@users.noreply.github.com>
2026-05-27 11:22:32 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,31 @@
-import { app, BrowserWindow, desktopCapturer, session } from "electron";
+import {
+	app,
+	BrowserWindow,
+	desktopCapturer,
+	session
+} from "electron";
 import http from "node:http";
-import { env } from "node:process";
+import {
+	env
+} from "node:process";
 
-const { PASSWORD = "", USERNAME = "", PORT, TUNNEL_URL } = env;
+const {
+	PASSWORD = "", USERNAME = "", PORT, TUNNEL_URL
+} = env;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/log") {
-    console.log("[renderer]", req.url);
-    res.writeHead(204);
-    res.end();
-    return;
-  }
+	if (req.url === "/log") {
+		console.log("[renderer]", req.url);
+		res.writeHead(204);
+		res.end();
+		return;
+	}
 
-  if (req.url === "/host") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`
+	if (req.url === "/host") {
+		res.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+		res.end(`
 <!DOCTYPE html>
 <html>
 <body>
@@ -24,12 +35,12 @@ const server = http.createServer((req, res) => {
   const peer = new Peer("${TUNNEL_URL}");
 
   const stream = await navigator.mediaDevices.getDisplayMedia({
-    video: true,
-    audio: true
+	video: true,
+	audio: true
   });
 
   peer.on("call", (call) => {
-    call.answer(stream);
+	call.answer(stream);
   });
 
 })();
@@ -37,12 +48,14 @@ const server = http.createServer((req, res) => {
 </body>
 </html>
 `);
-    return;
-  }
+		return;
+	}
 
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`
+	if (req.url === "/") {
+		res.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+		res.end(`
 <!DOCTYPE html>
 <html>
 <body>
@@ -57,34 +70,45 @@ peer.on("open", () => {
   const call = peer.call("${TUNNEL_URL}", null);
 
   call.on("stream", (stream) => {
-    video.srcObject = stream;
+	video.srcObject = stream;
   });
 });
 </script>
 </body>
 </html>
 `);
-    return;
-  }
+		return;
+	}
 
-  res.writeHead(404);
-  res.end();
+	res.writeHead(404);
+	res.end();
 });
 
 await app.whenReady();
 console.log("b")
-const win = new BrowserWindow({ show: false });
+const win = new BrowserWindow({
+	show: false
+});
 
 session.defaultSession.setDisplayMediaRequestHandler(async (request, callback) => {
-  const sources = await desktopCapturer.getSources({ types: ["screen"] });
-  callback({
-    video: sources?.[0],
-    audio: "loopback"
-  });
-}, { useSystemPicker: false });
+	const sources = await desktopCapturer.getSources({
+		types: ["screen"]
+	});
+	const source = sources?.[0];
+	if (source) {
+		callback({
+			video: source,
+			audio: "loopback"
+		});
+	} else {
+		callback({});
+	}
+}, {
+	useSystemPicker: false
+});
 
 await win.loadURL(`http://localhost:${PORT}/host`);
 
 server.listen(PORT, () => {
-  console.log(TUNNEL_URL);
+	console.log(TUNNEL_URL);
 });
