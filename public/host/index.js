1b6772ea6ade9bfdd27fad472e7bad423edfae8c
a <102840087+kingdudely@users.noreply.github.com>
2026-05-29 14:48:31 -0400

    Create index.js

---
+++ public/host/index.js
@@ -0,0 +1,37 @@
+const peer = new Peer("host", {
+	host: "localhost",
+	port: PORT,
+	path: "/peerjs",
+	secure: window.isSecureContext
+});
+
+console.log("A");
+
+peer.on('call', async (call) => {
+	console.log("B");
+	try {
+		const stream = await navigator.mediaDevices.getDisplayMedia({
+			video: true,
+			audio: false
+		});
+		console.log("C");
+		call.answer(stream);
+		console.log("D");
+	} catch (err) {
+		console.log(`WebRTC stream generation exception raised: ${err.message || err.toString()}`);
+	}
+});
+
+console.log("E");
+
+peer.on('connection', (conn) => {
+	console.log("F");
+	conn.on('data', (data) => {
+		// console.log("G");
+		window[data.type]?.(...data.args);
+		// console.log("H");
+	});
+	console.log("I");
+});
+
+console.log("J");
