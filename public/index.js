d606c018dae52a7691bfe764a1be808a77c98e3b
a <102840087+kingdudely@users.noreply.github.com>
2026-05-29 11:34:35 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -13,6 +13,20 @@ let dataConnection;
 
 peer.on("open", (id) => {
 	dataConnection = peer.connect('host');
+
+	dataConnection.on('open', () => {
+		console.log("Data connection established. Starting video stream call...");
+
+		const call = peer.call('host', new MediaStream());
+		
+		call.on('stream', (remoteStream) => {
+			console.log("Received remote stream!");
+			remoteVideo.srcObject = remoteStream;
+			remoteVideo.play().catch(console.error);
+		});
+
+		call.on('error', console.error);
+	});
 });
 
 function sendEventData(type, args) {
@@ -39,11 +53,4 @@ remoteVideo.addEventListener('mouseup', (event) => {
 remoteVideo.addEventListener('keydown', (event) => {
 	event.preventDefault();
 	sendEventData(event.type, [event.key]);
-});
-
-const call = peer.call('host', new MediaStream());
-		
-call.on('stream', (remoteStream) => {
-	remoteVideo.srcObject = remoteStream;
-	remoteVideo.play().catch(console.error);
 });
