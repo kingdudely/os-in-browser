dabf6437a7b4dfcee3bf58c30a6a3f068afe747c
a <102840087+kingdudely@users.noreply.github.com>
2026-05-30 22:43:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,36 +7,36 @@ port = 8080
 screenshare_options = {"framerate": "30"}
 
 match platform:
-    case "linux":
-        screenshare = MediaPlayer(environ["DISPLAY"], format="x11grab", options=screenshare_options) # ":0.0"
-    case "darwin":
-        screenshare = MediaPlayer("Capture screen 0", format="avfoundation", options=screenshare_options)
-    case "win32":
-        screenshare = MediaPlayer("desktop", format="gdigrab", options=screenshare_options)
-    case _:
-        raise RuntimeError(f"Unsupported platform: {platform}")
+	case "linux":
+		screenshare = MediaPlayer(environ["DISPLAY"], format="x11grab", options=screenshare_options) # ":0.0"
+	case "darwin":
+		screenshare = MediaPlayer("Capture screen 0", format="avfoundation", options=screenshare_options)
+	case "win32":
+		screenshare = MediaPlayer("desktop", format="gdigrab", options=screenshare_options)
+	case _:
+		raise RuntimeError(f"Unsupported platform: {platform}")
 
 app = web.Application()
 routes = web.RouteTableDef()
 
 @routes.get("/")
 async def index(request):
-    return web.FileResponse("index.html")
+	return web.FileResponse("index.html")
 
 @routes.post("/whip")
 async def whip(request):
 	from aiortc import RTCPeerConnection, RTCSessionDescription
-    sdp = await request.text()
-    peer = RTCPeerConnection()
-    peer.addTrack(screenshare.video)
-    await peer.setRemoteDescription(RTCSessionDescription(sdp=sdp, type="offer"))
-    answer = await peer.createAnswer()
-    await peer.setLocalDescription(answer)
-    return web.Response(text=pc.localDescription.sdp, content_type="application/sdp", status=201)
+	sdp = await request.text()
+	peer = RTCPeerConnection()
+	peer.addTrack(screenshare.video)
+	await peer.setRemoteDescription(RTCSessionDescription(sdp=sdp, type="offer"))
+	answer = await peer.createAnswer()
+	await peer.setLocalDescription(answer)
+	return web.Response(text=pc.localDescription.sdp, content_type="application/sdp", status=201)
 
 app.add_routes(routes)
 
 if __name__ == "__main__":
-    from pycloudflared import try_cloudflared
+	from pycloudflared import try_cloudflared
 	try_cloudflared(port=port, verbose=True)
 	web.run_app(app, port=port)
