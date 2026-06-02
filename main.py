df44a76137efdea006c99a0d00bf4514dcaab9fd
a <102840087+kingdudely@users.noreply.github.com>
2026-06-02 08:01:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-from os import environ
+from os import getenv
 from sys import platform
 from aiohttp import web
 from aiortc.contrib.media import MediaPlayer
 
-port = int(environ["PORT"])
-screenshare_options = {"framerate": "30"}
-
 match platform:
 	case "linux":
-		def get_screenshare(options):
-			return MediaPlayer(environ["DISPLAY"], format="x11grab", options=options) # :0.0
+		def get_screenshare(**options):
+			return MediaPlayer(getenv("DISPLAY", ":0"), format="x11grab", options=options) # :0.0
 	case "darwin":
-		def get_screenshare(options):
+		def get_screenshare(**options):
 			return MediaPlayer("Capture screen 0", format="avfoundation", options=options)
 	case "win32":
-		def get_screenshare(options):
+		def get_screenshare(**options):
 			return MediaPlayer("desktop", format="gdigrab", options=options)
 	case _:
 		raise RuntimeError(f"Unsupported platform: {platform}")
 
-screenshare = get_screenshare(screenshare_options)
+screenshare = get_screenshare(framerate="30")
 app = web.Application()
 routes = web.RouteTableDef()
 
-@routes.get("/")
-async def index(request):
-	return web.FileResponse("index.html")
+routes.static('/', './public', show_index=True)
 
 @routes.post("/whip")
 async def whip(request):
@@ -41,4 +36,9 @@ async def whip(request):
 app.add_routes(routes)
 
 if __name__ == "__main__":
-	web.run_app(app, port=port)
+	from with_cloudflared import cloudflared
+	port = 8080
+
+	with cloudflared(port=port) as cloudflared_address:
+		print(cloudflared_address)
+		web.run_app(app, port=port)
