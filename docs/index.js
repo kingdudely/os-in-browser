2d7f5559b9cd7fbb5d65aad1c31b5ceff75d801b
a <102840087+kingdudely@users.noreply.github.com>
2026-06-20 01:29:31 -0400

    d

--- docs/index.js
+++ docs/index.js
@@ -1,32 +1,40 @@
-import Peer from "./peer.js";
-import nutKeyMap from './nutKeyMap.json' with { type: 'json' };
-import nutButtonMap from './nutButtonMap.json' with { type: 'json' };
+import codeMap from "./code-map.json" with { type: "json" };
 
-/*
-IOHIDUserDevice or HIDVirtualDevice for macos (whatever doesn't require payment or something complicated, just sudo)
-/dev/uinput or /dev/uhid for linux
-HidP_TranslateUsagesToI8042ScanCodes for windows (or maybe Interception or vjoy or vigembus, or a different signed kernel driver)
-*/
 async function enableImmersiveMode(target) {
 	if (document.fullscreenEnabled && !document.fullscreenElement) {
-		await document.body.requestFullscreen({ // target.body
-			"navigationUI": "hide" 
+		await document.body.requestFullscreen({ // target
+			"navigationUI": "hide"
 		});
 	};
 
 	if (!document.pointerLockElement) {
 		await target.requestPointerLock({
 			"unadjustedMovement": true
-        });
+		});
 	}
 }
 
 const screenshare = document.getElementById("screenshare");
-const sharedBuffer = new ArrayBuffer(4);
-const sharedBytes = new Uint8Array(sharedBuffer);
-const sharedView = new DataView(sharedBuffer);
 
-const peer = new Peer();
+// shared buffer: 16 (max ip) + 2 (port) + 32 (fingerprint) = 50
+const sharedBytes = new Uint8Array(50);
+const sharedView = new DataView(sharedBytes.buffer);
+const certificate = await RTCPeerConnection.generateCertificate({ name: "ECDSA", namedCurve: "P-256" });
+const fingerprint = certificate.getFingerprints().find(f => f.algorithm === "sha-256").value;
+const fingerprintBytes = Uint8Array.fromHex(fingerprint.replace(/:/g, ''));
+const usernameFragment = "myufraghere1234";
+const password = "mypasswordthatisverylong12345";
+const streamerFingerprint = "";
+
+const peer = new RTCPeerConnection({
+	iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+	certificates: [certificate]
+});
+
+peer.addEventListener("track", (event) => {
+	screenshare.srcObject = event.streams[0];
+});
+
 peer.addTransceiver("audio", { direction: "recvonly" });
 peer.addTransceiver("video", { direction: "recvonly" });
 
@@ -49,18 +57,102 @@ const keyboardChannel = peer.createDataChannel("keyboard", {
 	id: 2
 });
 
-await navigator.clipboard.writeText(await peer.getShareId());
+const shareIdOffer = await (async function () {
+	const offer = await peer.createOffer({
+		offerToReceiveAudio: true,
+		offerToReceiveVideo: true
+	})
+
+	offer.sdp = offer.sdp
+		.replace(/a=ice-ufrag:\S+/g, `a=ice-ufrag:${usernameFragment}`)
+		.replace(/a=ice-pwd:\S+/g, `a=ice-pwd:${password}`);
+
+	await peer.setLocalDescription(offer);
+
+	const { address, port } = await new Promise((resolve) => {
+		peer.addEventListener("icecandidate", function onCandidate({ candidate }) {
+			if (candidate?.type === "srflx") {
+				peer.removeEventListener("icecandidate", onCandidate);
+				resolve(candidate);
+			}
+		});
+	});
+
+	let offset = 0;
+	const isIPv6 = address.includes(':');
+
+	if (isIPv6) {
+		address.split(':').forEach((hextet, index) => sharedView.setUint16(index * 2, parseInt(hextet, 16), true));
+		offset += 16;
+	} else {
+		address.split('.').forEach((octet, index) => sharedView.setUint8(index, parseInt(octet, 10)));
+		offset += 4;
+	}
+
+	sharedView.setUint16(offset, port, true);
+	offset += 2;
+	sharedBytes.set(fingerprintBytes, offset);
+	offset += 32;
+
+	return sharedBytes.subarray(0, offset).toBase64({ alphabet: "base64url" });
+})();
+
+await navigator.clipboard.writeText(shareIdOffer);
 window.alert("Copied the share ID into your clipboard");
 const streamerShareId = window.prompt("Share ID response:");
-await peer.connectToShareId(streamerShareId)
 
-peer.addEventListener("track", (event) => {
-	screenshare.srcObject = event.streams[0];
-});
+await (async function connectToStreamerShareId() {
+	let offset = 0;
+	const bytes = Uint8Array.fromBase64(streamerShareId, { alphabet: "base64url" });
+	const view = new DataView(bytes.buffer);
+	const isIPv6 = bytes.length === 18;
+	const address = isIPv6
+		? (offset += 16, bytes.subarray(0, 16).toHex().match(/.{1,4}/g).join(':'))
+		: (offset += 4, bytes.subarray(0, 4).join("."));
+
+	const port = view.getUint16(offset, true);
+	offset += 2;
+
+	if (offset !== bytes.length) {
+		throw new Error("Couldn't connect to share ID");
+	}
+
+	const commonIceLines = [
+		`c=IN IP4 0.0.0.0`,
+		`a=ice-ufrag:${usernameFragment}`,
+		`a=ice-pwd:${password}`,
+		`a=fingerprint:sha-256 ${streamerFingerprint}`,
+		`a=setup:active`,
+		`a=candidate:1 1 udp 1686052607 ${address} ${port} typ srflx`,
+	];
+
+	const streamerSdp = [
+		"v=0",
+		"o=- 0 0 IN IP4 127.0.0.1",
+		"s=-",
+		"t=0 0",
+		"a=group:BUNDLE 0 1 2",
+
+		"m=audio 9 UDP/TLS/RTP/SAVPF 111",
+		...commonIceLines,
+		`a=sendonly`, "a=mid:0", "a=rtcp-mux", "a=rtpmap:111 opus/48000/2",
+
+		"m=video 9 UDP/TLS/RTP/SAVPF 96",
+		...commonIceLines,
+		`a=sendonly`, "a=mid:1", "a=rtcp-mux", "a=rtpmap:96 AV1/90000",
+
+		"m=application 9 UDP/DTLS/SCTP webrtc-datachannel",
+		...commonIceLines,
+		"a=mid:2", "a=sctp-port:5000", "a=max-message-size:262144",
+		"",
+	].join("\r\n");
+
+	await peer.setRemoteDescription({ type: "answer", sdp: streamerSdp });
+})()
 
 screenshare.addEventListener("pointermove", (event) => { // pointerrawupdate - safari doesn't support unfortunately (I wish MacOS had touchscreen and stylus APIs)
 	if (pointerMovementChannel.readyState !== "open") return;
-	// make flag to say if it is clientX or movementX
+	// make flag to say if it is clientX or movementX with document.pointerLockElement
 	sharedView.setInt16(0, event.movementX, true);
 	sharedView.setInt16(2, event.movementY, true);
 	pointerMovementChannel.send(sharedBytes.subarray(0, 4));
@@ -70,27 +162,15 @@ screenshare.addEventListener("pointerdown", (event) => {
 	if (pointerClickChannel.readyState !== "open") return;
 	enableImmersiveMode(screenshare).catch(console.warn);
 
-	const nutButton = nutButtonMap.indexOf(event.button);
-	if (nutButton === -1) {
-		console.warn("Could not find Nut.JS Button equivalent");
-		return;
-	}
-
 	sharedView.setUint8(0, 1); // isDown
-	sharedView.setUint8(1, nutButton);
+	sharedView.setUint8(1, event.button);
 	pointerClickChannel.send(sharedBytes.subarray(0, 2));
 });
 
 screenshare.addEventListener("pointerup", (event) => {
 	if (pointerClickChannel.readyState !== "open") return;
-	const nutButton = nutButtonMap.indexOf(event.button);
-	if (nutButton === -1) {
-		console.warn("Could not find Nut.JS Button equivalent");
-		return;
-	}
-
 	sharedView.setUint8(0, 0); // isDown
-	sharedView.setUint8(1, nutButton);
+	sharedView.setUint8(1, event.button);
 	pointerClickChannel.send(sharedBytes.subarray(0, 2));
 });
 
@@ -98,27 +178,27 @@ screenshare.addEventListener("keydown", (event) => {
 	if (keyboardChannel.readyState !== "open" || event.repeat || !event.code) return;
 	enableImmersiveMode(screenshare).catch(console.warn);
 
-	const nutKey = nutKeyMap.indexOf(event.code);
-	if (nutKey === -1) {
-		console.warn("Could not find Nut.JS Key equivalent");
+	const codeIndex = codeMap.indexOf(event.code);
+	if (codeIndex === -1) {
+		console.warn(`${event.code} is not supported`);
 		return;
 	}
 
 	sharedView.setUint8(0, 1); // isDown
-	sharedView.setUint8(1, nutKey);
+	sharedView.setUint8(1, codeIndex);
 	keyboardChannel.send(sharedBytes.subarray(0, 2));
 });
 
 screenshare.addEventListener("keyup", (event) => {
 	if (keyboardChannel.readyState !== "open" || !event.code) return;
 
-	const nutKey = nutKeyMap.indexOf(event.code);
-	if (nutKey === -1) {
-		console.warn("Could not find Nut.JS Key equivalent");
+	const codeIndex = codeMap.indexOf(event.code);
+	if (codeIndex === -1) {
+		console.warn(`${event.code} is not supported`);
 		return;
 	}
 
 	sharedView.setUint8(0, 0); // isDown
-	sharedView.setUint8(1, nutKey);
+	sharedView.setUint8(1, codeIndex);
 	keyboardChannel.send(sharedBytes.subarray(0, 2));
 })
