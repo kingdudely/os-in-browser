6898d1614c7309357289f57332a6d18020846571
a <102840087+kingdudely@users.noreply.github.com>
2026-06-27 23:58:50 -0400

    yes

--- main.go
+++ main.go
@@ -160,7 +160,7 @@ func main() {
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, indexHTML, ip, port, fpHex, ip, port)
+		fmt.Fprintf(w, indexHTML, fpHex, ip, port)
 	})
 
 	mux.HandleFunc("/wt", func(w http.ResponseWriter, r *http.Request) {
