f04017c7742f30a75a0bdc4d239e2f48cb4d6a3c
a <102840087+kingdudely@users.noreply.github.com>
2026-07-08 15:00:58 -0400

    ninie

--- app/index.js
+++ app/index.js
@@ -29,110 +29,114 @@ if (username && password) {
 
 app.use(...middleware);
 
-app.post('/whip', express.text({ type: 'application/sdp' }), async (req, res) => {
-	const offer = req.body;
+(async function main() {
+	const stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: false });
+	const tracks = stream.getTracks();
 
-	res.status(201)
-		.set('Content-Type', 'application/sdp')
-		.send(await createAnswer(offer));
-});
+	async function createAnswer(offer) {
+		const peer = new RTCPeerConnection({
+			iceServers: [
+				{ urls: "stun:stun.l.google.com:19302" },
+				{ urls: "stun:stun.cloudflare.com:3478" }
+			]
+		});
+		
+		tracks.forEach((track) => peer.addTrack(track, stream));
 
-const stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: false });
-const tracks = stream.getTracks();
+		const pointerMovementChannel = peer.createDataChannel("pointer-movement", {
+			ordered: false,
+			maxRetransmits: 0,
+			negotiated: true,
+			id: 0
+		});
 
-async function createAnswer(offer) {
-	const peer = new RTCPeerConnection({
-		iceServers: [
-			{ urls: "stun:stun.l.google.com:19302" },
-			{ urls: "stun:stun.cloudflare.com:3478" }
-		]
+		pointerMovementChannel.addEventListener("message", async (event) => {
+			const view = new DataView(event.data);
+			const isRelative = view.byteLength === 4;
+			let absoluteX = 0;
+			let absoluteY = 0;
+
+			if (isRelative) {
+				const movementX = view.getInt16(0, true);
+				const movementY = view.getInt16(2, true);
+				const currentPosition = await mouse.getPosition();
+				absoluteX = currentPosition.x + movementX;
+				absoluteY = currentPosition.y + movementY;
+			} else {
+				absoluteX = view.getUint32(0, true);
+				absoluteY = view.getUint32(4, true);
+			}
+
+			await mouse.setPosition(new Point(x, y));
+		});
+
+		const pointerClickChannel = peer.createDataChannel("pointer-click", {
+			ordered: true,
+			negotiated: true,
+			id: 1
+		});
+
+		pointerClickChannel.addEventListener("message", async (event) => {
+			const view = new DataView(event.data);
+			const isDown = view.getUint8(0) === 1;
+			const button = view.getUint8(1);
+
+			if (isDown) {
+				await mouse.pressButton(button);
+			} else {
+				await mouse.releaseButton(button);
+			}
+		});
+
+		const keyboardTypeChannel = peer.createDataChannel("keyboard-type", {
+			ordered: true,
+			negotiated: true,
+			id: 2
+		});
+
+		keyboardTypeChannel.addEventListener("message", async (event) => {
+			const view = new DataView(event.data);
+			const isDown = view.getUint8(0) === 1;
+			const key = view.getUint8(1);
+
+			if (isDown) {
+				await keyboard.pressKey(key);
+			} else {
+				await keyboard.releaseKey(key);
+			}
+		});
+
+		await peer.setRemoteDescription({ type: "offer", sdp: offer });
+		await peer.setLocalDescription();
+
+		await new Promise((resolve) => {
+			if (peer.iceGatheringState === "complete") {
+				resolve();
+			} else {
+				peer.addEventListener("icegatheringstatechange", function onStateChange() {
+					if (peer.iceGatheringState === "complete") {
+						peer.removeEventListener("icegatheringstatechange", onStateChange);
+						resolve();
+					}
+				});
+			}
+		});
+
+		return peer.localDescription.sdp;
+	}
+
+	app.post('/whip', express.text({ type: 'application/sdp' }), async (req, res) => {
+		const offer = req.body;
+
+		res.status(201)
+			.set('Content-Type', 'application/sdp')
+			.send(await createAnswer(offer));
 	});
-	
-	tracks.forEach((track) => peer.addTrack(track, stream));
 
-	const pointerMovementChannel = peer.createDataChannel("pointer-movement", {
-		ordered: false,
-		maxRetransmits: 0,
-		negotiated: true,
-		id: 0
+	const tunnel = await startTunnel({
+		port: server.address().port,
+		acceptCloudflareNotice: true
 	});
 
-	pointerMovementChannel.addEventListener("message", async (event) => {
-		const view = new DataView(event.data);
-		const isRelative = view.byteLength === 4;
-		let absoluteX = 0;
-		let absoluteY = 0;
-
-		if (isRelative) {
-			const movementX = view.getInt16(0, true);
-			const movementY = view.getInt16(2, true);
-			const currentPosition = await mouse.getPosition();
-			absoluteX = currentPosition.x + movementX;
-			absoluteY = currentPosition.y + movementY;
-		} else {
-			absoluteX = view.getUint32(0, true);
-			absoluteY = view.getUint32(4, true);
-		}
-
-		await mouse.setPosition(new Point(x, y));
-	});
-
-	const pointerClickChannel = peer.createDataChannel("pointer-click", {
-		ordered: true,
-		negotiated: true,
-		id: 1
-	});
-
-	pointerClickChannel.addEventListener("message", async (event) => {
-		const view = new DataView(event.data);
-		const isDown = view.getUint8(0) === 1;
-		const button = view.getUint8(1);
-
-		if (isDown) {
-			await mouse.pressButton(button);
-		} else {
-			await mouse.releaseButton(button);
-		}
-	});
-
-	const keyboardTypeChannel = peer.createDataChannel("keyboard-type", {
-		ordered: true,
-		negotiated: true,
-		id: 2
-	});
-
-	keyboardTypeChannel.addEventListener("message", async (event) => {
-		const view = new DataView(event.data);
-		const isDown = view.getUint8(0) === 1;
-		const key = view.getUint8(1);
-
-		if (isDown) {
-			await keyboard.pressKey(key);
-		} else {
-			await keyboard.releaseKey(key);
-		}
-	});
-
-	await peer.setRemoteDescription({ type: "offer", sdp: offer });
-	await peer.setLocalDescription();
-
-	await new Promise((resolve) => {
-		if (peer.iceGatheringState === "complete") {
-			resolve();
-		} else {
-			peer.addEventListener("icegatheringstatechange", function onStateChange() {
-				if (peer.iceGatheringState === "complete") {
-					peer.removeEventListener("icegatheringstatechange", onStateChange);
-					resolve();
-				}
-			});
-		}
-	});
-
-	return peer.localDescription.sdp;
-}
-
-startTunnel({
-	port: server.address().port,
-	acceptCloudflareNotice: true
-}).then((tunnel) => console.log(`Your URL: ${tunnel.url}`));
+	console.log(`Your URL: ${tunnel.url}`);
+})();
