eedfe960c18833c5540b708ef01a978d03eab865
a <102840087+kingdudely@users.noreply.github.com>
2026-07-08 13:39:50 -0400

    nice

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { app, BrowserWindow, desktopCapturer, session } from 'electron';
 import { fileURLToPath } from 'node:url';
-
+console.log("A")
 await app.whenReady();
 console.log("B")
 session.defaultSession.setDisplayMediaRequestHandler(async (request, callback) => {
