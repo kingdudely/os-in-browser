7e1f97cfc848ec085dd549ac0e167e255ad207a5
a <102840087+kingdudely@users.noreply.github.com>
2026-07-13 00:14:09 -0400

    nice boi

--- public/index.js
+++ public/index.js
@@ -9,10 +9,8 @@ window.addEventListener("unhandledrejection", (event) => {
 });
 
 import codeMap from "./code-map.json" with { type: "json" };
-import constants from "./constants.json" with { type: "json" }; // type: "text"
-const { password, usernameFragment, workflowFingerprint } = constants;
 
-function enableImmersiveMode() {
+function triggerImmersiveMode() {
 	if (document.fullscreenEnabled && !document.fullscreenElement) {
 		document.body.requestFullscreen({ // target, await
 			"navigationUI": "hide"
@@ -27,11 +25,14 @@ function enableImmersiveMode() {
 }
 
 const screenshare = document.getElementById("screenshare");
-const sharedBytes = new Uint8Array(5); 
+const sharedBytes = new Uint8Array(8); 
 const sharedView = new DataView(sharedBytes.buffer);
 
 const peer = new RTCPeerConnection({
-	iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+	iceServers: [
+		{ urls: "stun:stun.l.google.com:19302" },
+		{ urls: "stun:stun.cloudflare.com:3478" }
+	]
 });
 
 peer.addEventListener("track", (event) => {
@@ -73,127 +74,54 @@ const scrollChannel = peer.createDataChannel("scroll", {
     id: 4
 });
 
-const offer = await peer.createOffer({
-	offerToReceiveAudio: true,
-	offerToReceiveVideo: true
+await peer.setLocalDescription();
+
+await new Promise((resolve) => {
+	function checkState() {
+		if (peer.iceGatheringState === "complete") {
+			peer.removeEventListener('icegatheringstatechange', checkState);
+			resolve();
+		}
+	}
+
+	checkState();
+	peer.addEventListener('icegatheringstatechange', checkState);
 });
-
-const offerSdp = offer.sdp
-	.replace(/a=ice-ufrag:\S+/g, `a=ice-ufrag:${usernameFragment}`)
-	.replace(/a=ice-pwd:\S+/g, `a=ice-pwd:${password}`);
-
-await peer.setLocalDescription({ type: "offer", sdp: offerSdp });
-
-const browserAddress = await new Promise((resolve, reject) => {
-    function onCandidate({ candidate }) {
-        if (candidate?.type === "srflx") {
-            peer.removeEventListener("icecandidate", onCandidate);
-            peer.removeEventListener("icegatheringstatechange", onGatheringChange);
-            resolve(`${candidate.address}:${candidate.port}`);
-        }
-    }
-
-    function onGatheringChange() {
-        if (peer.iceGatheringState === "complete") {
-            peer.removeEventListener("icegatheringstatechange", onGatheringChange);
-            peer.removeEventListener("icecandidate", onCandidate);
-            reject(new Error("ICE gathering complete, but no srflx candidate found."));
-        }
-    }
-
-    peer.addEventListener("icecandidate", onCandidate);
-    peer.addEventListener("icegatheringstatechange", onGatheringChange);
-});
-
-const [runnerAddress, runnerPort] = await new Promise((resolve, reject) => {
-    const connectDialog = document.getElementById("connect-dialog");
-    connectDialog.showModal();
-
-    document.getElementById("copy-offer").addEventListener("click", () => {
-		navigator.clipboard.writeText(browserAddress)
-			.then(() => window.alert("Copied offer to clipboard"))
-			.catch(reject);
-	});
-
-    document.getElementById("runner-submit").addEventListener("click", () => {
-		const value = document.getElementById("runner-input").value.trim();
-        if (value) {
-            connectDialog.close();
-			const url = new URL(`http://${value}`);
-            resolve([url.hostname, url.port]);
-        }
-	});
-});
-
-const commonIceLines = [
-    `c=IN IP4 ${runnerAddress}`,
-    `a=ice-ufrag:${usernameFragment}`,
-    `a=ice-pwd:${password}`,
-    `a=fingerprint:sha-256 ${workflowFingerprint}`,
-    "a=setup:active",
-    `a=candidate:0 1 UDP 1686052607 ${runnerAddress} ${runnerPort} typ srflx`
-];
 
 await peer.setRemoteDescription({
 	type: "answer",
-	sdp: [
-		"v=0",
-		"o=- 0 0 IN IP4 0.0.0.0",
-		"s=-",
-		"t=0 0",
-		"a=group:BUNDLE 0 1 2",
-
-		"m=audio 9 UDP/TLS/RTP/SAVPF 111",
-		...commonIceLines,
-		"a=sendonly",
-		"a=mid:0",
-		"a=rtcp-mux",
-		"a=rtpmap:111 opus/48000/2",
-
-		"m=video 9 UDP/TLS/RTP/SAVPF 102",
-		...commonIceLines,
-		"a=sendonly",
-		"a=mid:1",
-		"a=rtcp-mux",
-		"a=rtpmap:102 H264/90000",
-
-		"m=application 9 UDP/DTLS/SCTP webrtc-datachannel",
-		...commonIceLines,
-		"a=mid:2",
-		"a=sctp-port:5000",
-		"a=max-message-size:262144",
-		"",
-	].join("\r\n")
+	sdp: answer
 });
 
-screenshare.addEventListener("pointermove", (event) => {
+window.addEventListener("pointermove", (event) => {
 	event.preventDefault();
 	if (pointerMovementChannel.readyState !== "open") return;
 
+	let packetSize = 0;
 	if (document.pointerLockElement) {
-		sharedView.setUint8(0, 1); // isRelative
-		sharedView.setInt16(1, event.movementX, true);
-		sharedView.setInt16(3, event.movementY, true);
+		sharedView.setInt16(0, event.movementX, true);
+		sharedView.setInt16(2, event.movementY, true);
+		packetSize = 4;
 	} else {
-		sharedView.setUint8(0, 0); // isRelative
-		sharedView.setInt16(1, event.clientX, true);
-		sharedView.setInt16(3, event.clientY, true);
+		sharedView.setUint32(0, event.clientX, true);
+		sharedView.setUint32(4, event.clientY, true);
+		packetSize = 8;
 	}
 	
-	pointerMovementChannel.send(sharedBytes.subarray(0, 5));
+	pointerMovementChannel.send(sharedBytes.subarray(0, packetSize));
 });
 
-screenshare.addEventListener("pointerdown", (event) => {
+window.addEventListener("pointerdown", (event) => {
 	event.preventDefault();
 	if (pointerClickChannel.readyState !== "open") return;
-	enableImmersiveMode();
+	triggerImmersiveMode();
 
 	sharedView.setUint8(0, 1); // isDown
 	sharedView.setUint8(1, event.button);
 	pointerClickChannel.send(sharedBytes.subarray(0, 2));
 });
 
-screenshare.addEventListener("pointerup", (event) => {
+window.addEventListener("pointerup", (event) => {
 	event.preventDefault();
 	if (pointerClickChannel.readyState !== "open") return;
 
@@ -205,7 +133,7 @@ screenshare.addEventListener("pointerup", (event) => {
 window.addEventListener("keydown", (event) => { // screenshare
 	event.preventDefault();
 	if (keyboardChannel.readyState !== "open" || event.repeat || !event.code) return;
-	enableImmersiveMode();
+	triggerImmersiveMode();
 
 	const codeIndex = codeMap.indexOf(event.code);
 	if (codeIndex === -1) {
@@ -233,15 +161,18 @@ window.addEventListener("keyup", (event) => {
 	keyboardChannel.send(sharedBytes.subarray(0, 2));
 });
 
-window.addEventListener("resize", () => {
-    if (screenResizeChannel.readyState !== "open") return;
+function onResize() {
+	if (screenResizeChannel.readyState !== "open") return;
 
-    sharedView.setUint16(0, window.innerWidth, true);
-    sharedView.setUint16(2, window.innerHeight, true);
+    sharedView.setUint32(0, window.innerWidth, true);
+    sharedView.setUint32(4, window.innerHeight, true);
     screenResizeChannel.send(sharedBytes.subarray(0, 4));
-});
+}
 
-screenshare.addEventListener("wheel", (event) => {
+screenResizeChannel.addEventListener("open", onResize);
+window.addEventListener("resize", onResize);
+
+window.addEventListener("wheel", (event) => {
     event.preventDefault();
     if (scrollChannel.readyState !== "open") return;
 
@@ -255,7 +186,7 @@ screenshare.addEventListener("wheel", (event) => {
 		}
 	})();
 
-    sharedView.setInt16(0, event.deltaX * multiplier, true);
-    sharedView.setInt16(2, event.deltaY * multiplier, true);
-    scrollChannel.send(sharedBytes.subarray(0, 4));
+    sharedView.setFloat32(0, event.deltaX * multiplier, true);
+    sharedView.setFloat32(4, event.deltaY * multiplier, true);
+    scrollChannel.send(sharedBytes.subarray(0, 8));
 });
