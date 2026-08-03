b8c514356da021d7a58b441986599436db679aae
a <102840087+kingdudely@users.noreply.github.com>
2026-08-02 21:09:51 -0400

    ok

---
+++ index.js
@@ -0,0 +1,34 @@
+// Use IPC instead, make whep endpoint
+console.log("Node.js loaded!")
+import { app, BrowserWindow, desktopCapturer, session } from 'electron';
+import { fileURLToPath } from 'node:url';
+
+app.whenReady().then(() => {
+	console.log("App loaded!")
+	session.defaultSession.setDisplayMediaRequestHandler(async (request, callback) => {
+		const sources = await desktopCapturer.getSources({
+			types: ['screen']
+		});
+
+		callback({
+			video: sources[0],
+			audio: 'loopback'
+		});
+	}, { useSystemPicker: false });
+
+	const win = new BrowserWindow({
+		show: false,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			sandbox: false
+		}
+	});
+
+	win.webContents.on('console-message', ({ level, message, lineNumber, sourceId }) => {
+		console.log(`[renderer:${level}] ${message} (${sourceId}:${lineNumber})`);
+	});
+	win.loadFile(fileURLToPath(import.meta.resolve("./index.html")));
+});
+
+app.on('window-all-closed', () => app.quit());
