516d24eba3b6d86c4391b0862cbabc5be383dbd9
a <102840087+kingdudely@users.noreply.github.com>
2026-08-31 23:55:39 +0000

    ok

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ const express = require("express");
 const expressWs = require("express-ws");
 const basicAuth = require("express-basic-auth");
 const { setTimeout } = require("node:timers/promises");
-const Tunnel = require("firetunnel");
 
 import ServerPeer from "./ServerPeer.js";
 
@@ -14,10 +13,9 @@ const {
 	GITHUB_SHA,
 	USERNAME,
 	PASSWORD,
-	METRICS_HOSTNAME
+	METRICS_HOSTNAME,
+	PORT
 } = process.env;
-
-const port = 8080;
 
 const github = new Octokit();
 
@@ -40,43 +38,6 @@ app.use(express.static("./public"));
 
 app.ws("/", (ws, req) => new ServerPeer(ws));
 
-app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
-});
-
-await Tunnel.installCloudflared();
-
-const tunnel = new Tunnel({
-	url: `localhost:${port}`,
-	metrics: METRICS_HOSTNAME
-});
-
-while (!await tunnel.isReady())
-	await setTimeout(1000);
-
-const { hostname } = await tunnel.getQuickTunnelInfo();
-
-const deployments = await github.paginate(
-	github.rest.repos.listDeployments,
-	{
-		owner,
-		repo,
-		environment,
-		sha: GITHUB_SHA
-	}
-);
-
-const deployment = deployments[0];
-
-if (!deployment)
-	throw new Error("Deployment not found");
-
-await github.rest.repos.createDeploymentStatus({
-	owner,
-	repo,
-	environment,
-	deployment_id: deployment.id,
-	state: "in_progress",
-	description: "Remote desktop ready",
-	environment_url: `https://${hostname}`
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
 });
