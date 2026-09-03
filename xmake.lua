8b06f7621af633a7379abe7c99a35d7ef78cc9fb
a <102840087+kingdudely@users.noreply.github.com>
2026-09-03 18:09:20 +0000

    ok

--- xmake.lua
+++
@@ -1,7 +0,0 @@
-add_requires("screen_capture_lite")
-add_requires("x264")
-
-target("remote-desktop")
-    set_kind("binary")
-    add_files("src/*.cpp")
-    add_packages("screen_capture_lite", "x264")
