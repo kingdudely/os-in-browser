e8b1117dade5537e315c8e80e632a7b77be76580
a <102840087+kingdudely@users.noreply.github.com>
2026-09-03 18:54:33 +0000

    ok

--- src/macos/src/screen.mm
+++ src/macos/src/screen.mm
@@ -5,8 +5,6 @@
 #import <CoreMedia/CoreMedia.h>
 #import <CoreVideo/CoreVideo.h>
 
-#include <dispatch/dispatch.h>
-
 @interface CaptureOutput : NSObject <SCStreamOutput>
 
 @property(nonatomic, copy) void (^callback)(CMSampleBufferRef);
@@ -17,7 +15,7 @@
 
 - (void)stream:(SCStream*)stream
 didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
-       ofType:(SCStreamOutputType)type
+        ofType:(SCStreamOutputType)type
 {
     if (type != SCStreamOutputTypeScreen)
         return;
@@ -25,181 +23,149 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
     if (!CMSampleBufferIsValid(sampleBuffer))
         return;
 
-    if (!self.callback)
-        return;
-
-    self.callback(sampleBuffer);
+    if (self.callback)
+        self.callback(sampleBuffer);
 }
 
 @end
 
-void StartCapture(FrameCallback callback) {
-    dispatch_async(
-        dispatch_get_global_queue(
-            QOS_CLASS_USER_INTERACTIVE,
-            0
-        ),
-        ^{
-            @autoreleasepool {
-                dispatch_semaphore_t semaphore =
-                    dispatch_semaphore_create(0);
+static SCStream* g_stream = nil;
+static CaptureOutput* g_output = nil;
 
-                __block SCStream* stream = nil;
+void StartCapture(FrameCallback callback)
+{
+    [SCShareableContent
+        getShareableContentExcludingDesktopWindows:NO
+        onScreenWindowsOnly:YES
+        completionHandler:^(SCShareableContent* content, NSError* error)
+    {
+        if (error) {
+            NSLog(@"getShareableContent failed: %@", error);
+            return;
+        }
 
-                [SCShareableContent
-                    getShareableContentExcludingDesktopWindows:NO
-                    onScreenWindowsOnly:YES
-                    completionHandler:
-                    ^(SCShareableContent* content, NSError* error) {
+        if (content.displays.count == 0) {
+            NSLog(@"No displays found");
+            return;
+        }
 
-                        if (error) {
-                            NSLog(
-                                @"Failed to get shareable content: %@",
-                                error
-                            );
+        SCDisplay* display = content.displays[0];
 
-                            dispatch_semaphore_signal(semaphore);
-                            return;
-                        }
+        SCContentFilter* filter =
+            [[SCContentFilter alloc]
+                initWithDisplay:display
+                excludingApplications:@[]
+                exceptingWindows:@[]];
 
-                        if (content.displays.count == 0) {
-                            NSLog(@"No displays found");
+        SCStreamConfiguration* config =
+            [[SCStreamConfiguration alloc] init];
 
-                            dispatch_semaphore_signal(semaphore);
-                            return;
-                        }
+        config.width = display.width;
+        config.height = display.height;
 
-                        SCDisplay* display =
-                            content.displays[0];
+        config.pixelFormat = kCVPixelFormatType_32BGRA;
 
-                        SCContentFilter* filter =
-                            [[SCContentFilter alloc]
-                                initWithDisplay:display
-                                excludingApplications:@[]
-                                exceptingWindows:@[]];
+        config.minimumFrameInterval =
+            CMTimeMake(1, 60);
 
-                        SCStreamConfiguration* configuration =
-                            [[SCStreamConfiguration alloc] init];
+        config.queueDepth = 3;
+        config.showsCursor = YES;
+        config.capturesAudio = NO;
 
-                        configuration.width = display.width;
-                        configuration.height = display.height;
+        g_output = [[CaptureOutput alloc] init];
 
-                        // Directly request BGRA.
-                        configuration.pixelFormat =
-                            kCVPixelFormatType_32BGRA;
+        g_output.callback =
+            [callback = std::move(callback)]
+            (CMSampleBufferRef sampleBuffer)
+        {
+            CVPixelBufferRef buffer =
+                (CVPixelBufferRef)
+                CMSampleBufferGetImageBuffer(sampleBuffer);
 
-                        configuration.minimumFrameInterval =
-                            CMTimeMake(1, 60);
+            if (!buffer)
+                return;
 
-                        configuration.queueDepth = 3;
-                        configuration.showsCursor = YES;
+            CVPixelBufferLockBaseAddress(
+                buffer,
+                kCVPixelBufferLock_ReadOnly
+            );
 
-                        CaptureOutput* output =
-                            [[CaptureOutput alloc] init];
-
-                        output.callback =
-                            [callback](CMSampleBufferRef sampleBuffer) {
-
-                            CVPixelBufferRef buffer =
-                                CMSampleBufferGetImageBuffer(
-                                    sampleBuffer
-                                );
-
-                            if (!buffer)
-                                return;
-
-                            CVPixelBufferLockBaseAddress(
-                                buffer,
-                                kCVPixelBufferLock_ReadOnly
-                            );
-
-                            const auto* data =
-                                static_cast<const uint8_t*>(
-                                    CVPixelBufferGetBaseAddress(buffer)
-                                );
-
-                            if (data) {
-                                Frame frame{
-                                    data,
-                                    static_cast<int>(
-                                        CVPixelBufferGetWidth(buffer)
-                                    ),
-                                    static_cast<int>(
-                                        CVPixelBufferGetHeight(buffer)
-                                    ),
-                                    static_cast<int>(
-                                        CVPixelBufferGetBytesPerRow(buffer)
-                                    )
-                                };
-
-                                callback(frame);
-                            }
-
-                            CVPixelBufferUnlockBaseAddress(
-                                buffer,
-                                kCVPixelBufferLock_ReadOnly
-                            );
-                        };
-
-                        stream =
-                            [[SCStream alloc]
-                                initWithFilter:filter
-                                configuration:configuration
-                                delegate:nil];
-
-                        NSError* error = nil;
-
-                        BOOL added =
-                            [stream
-                                addStreamOutput:output
-                                type:SCStreamOutputTypeScreen
-                                sampleHandlerQueue:
-                                    dispatch_get_global_queue(
-                                        QOS_CLASS_USER_INTERACTIVE,
-                                        0
-                                    )
-                                error:&error];
-
-                        if (!added) {
-                            NSLog(
-                                @"Failed to add stream output: %@",
-                                error
-                            );
-
-                            dispatch_semaphore_signal(semaphore);
-                            return;
-                        }
-
-                        [stream
-                            startCaptureWithCompletionHandler:
-                            ^(NSError* error) {
-
-                            if (error) {
-                                NSLog(
-                                    @"Failed to start capture: %@",
-                                    error
-                                );
-                            } else {
-                                NSLog(
-                                    @"Started capture: %lux%lu",
-                                    (unsigned long)display.width,
-                                    (unsigned long)display.height
-                                );
-                            }
-
-                            dispatch_semaphore_signal(semaphore);
-                        }];
-                    }];
-
-                // Wait until startCapture has completed.
-                dispatch_semaphore_wait(
-                    semaphore,
-                    DISPATCH_TIME_FOREVER
+            const uint8_t* data =
+                static_cast<const uint8_t*>(
+                    CVPixelBufferGetBaseAddress(buffer)
                 );
 
-                // Keep the stream alive.
-                dispatch_main();
+            if (data) {
+                Frame frame{
+                    data,
+                    static_cast<int>(
+                        CVPixelBufferGetWidth(buffer)
+                    ),
+                    static_cast<int>(
+                        CVPixelBufferGetHeight(buffer)
+                    ),
+                    static_cast<int>(
+                        CVPixelBufferGetBytesPerRow(buffer)
+                    )
+                };
+
+                callback(frame);
             }
+
+            CVPixelBufferUnlockBaseAddress(
+                buffer,
+                kCVPixelBufferLock_ReadOnly
+            );
+        };
+
+        g_stream =
+            [[SCStream alloc]
+                initWithFilter:filter
+                configuration:config
+                delegate:nil];
+
+        NSError* addError = nil;
+
+        BOOL added =
+            [g_stream
+                addStreamOutput:g_output
+                type:SCStreamOutputTypeScreen
+                sampleHandlerQueue:
+                    dispatch_get_global_queue(
+                        QOS_CLASS_USER_INTERACTIVE,
+                        0
+                    )
+                error:&addError];
+
+        if (!added) {
+            NSLog(
+                @"addStreamOutput failed: %@",
+                addError
+            );
+
+            g_stream = nil;
+            g_output = nil;
+
+            return;
         }
-    );
+
+        [g_stream
+            startCaptureWithCompletionHandler:
+                ^(NSError* startError)
+        {
+            if (startError) {
+                NSLog(
+                    @"startCapture failed: %@",
+                    startError
+                );
+                return;
+            }
+
+            NSLog(
+                @"Screen capture started: %lux%lu",
+                (unsigned long)display.width,
+                (unsigned long)display.height
+            );
+        }];
+    }];
 }
