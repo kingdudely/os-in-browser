ea2a1bed14b48ecedcd57e34872a7a2d28ea62b7
a <102840087+kingdudely@users.noreply.github.com>
2026-09-03 15:24:15 +0000

    ok

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 // NWESM doesn't work .. :(, Do I use Electron?
 console.log("A")
-const { Octokit } = require("@octokit/action");
+import { Octokit } from "@octokit/action";
 const express = require("express");
 const expressWs = require("express-ws");
 const basicAuth = require("express-basic-auth");
